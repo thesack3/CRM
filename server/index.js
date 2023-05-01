325ad5f814705114ff4c4e9b2778483a165221cd
dominiq1 <dominiqmartinez13@gmail.com>
2023-05-01 15:03:05 -0700

    heroku main

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const colors = require("colors");
-require("dotenv").config();
+//  require("dotenv").config();
+
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
 const cors = require("cors");
