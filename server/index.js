1698340543d0f7bc88cc6e4f65017877914e1eb1
aliraza2u <raza8r@gmail.com>
2023-05-23 01:15:53 +0500

    created add lead rest endpoint

--- server/index.js
+++ server/index.js
@@ -16,12 +16,18 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 const bodyParser = require("body-parser");
+const Lead = require("./models/Lead");
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 //Connect to database
 connectDB();
 app.use(cors());
+
+// rest api for test route
+app.get("/lead", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.use(
   "/graphql",
@@ -30,6 +36,63 @@ app.use(
     graphiql: process.env.NODE_ENV === "development",
   })
 );
+
+// post route for add lead
+
+app.post("/addLead", async (req, res) => {
+  if (!req.body.firstName) return res.status(400).send("First Name is required");
+  if (!req.body.email) return res.status(400).send("Email is required");
+  // create lead
+  const findLead = await Lead.findOne({ email: req.body.email });
+  if (findLead) return res.status(400).send("Lead already exists");
+  const lead = await new Lead({
+    firstName: req.body.firstName,
+    email: req.body.email,
+    lastName: req.body.lastName,
+    phone: req.body.phone,
+    phoneStatus: req.body.phoneStatus,
+    description: req.body.description,
+    emailInvalid: req.body.emailInvalid,
+    GloballyOptedOutOfEmail: req.body.GloballyOptedOutOfEmail,
+    GloballyOptedOutOfBuyerAgentEmail: req.body.GloballyOptedOutOfBuyerAgentEmail,
+    GloballyOptedOutOfListingAgentEmail: req.body.GloballyOptedOutOfListingAgentEmail,
+    GloballyOptedOutOfLenderEmail: req.body.GloballyOptedOutOfLenderEmail,
+    GloballyOptedOutOfAlerts: req.body.GloballyOptedOutOfAlerts,
+    OptInDate: req.body.OptInDate,
+    BuyerAgentCategory: req.body.BuyerAgentCategory,
+    ListingAgentCategory: req.body.ListingAgentCategory,
+    LenderCategory: req.body.LenderCategory,
+    BuyerAgent: req.body.BuyerAgent,
+    ListingAgent: req.body.ListingAgent,
+    Lender: req.body.Lender,
+    OriginalSource: req.body.OriginalSource,
+    OriginalCampaign: req.body.OriginalCampaign,
+    LastAgentNote: req.body.LastAgentNote,
+    eAlerts: req.body.eAlerts,
+    VisitTotal: req.body.VisitTotal,
+    listingviewcount: req.body.listingviewcount,
+    AvgListingPrice: req.body.AvgListingPrice,
+    NextCallDue: req.body.NextCallDue,
+    LastAgentCallDate: req.body.LastAgentCallDate,
+    LastLenderCallDate: req.body.LastLenderCallDate,
+    FirstVisitDate: req.body.FirstVisitDate,
+    LastVisitDate: req.body.LastVisitDate,
+    RegisterDate: req.body.RegisterDate,
+    LeadType: req.body.LeadType,
+    AgentSelected: req.body.AgentSelected,
+    LenderOptIn: req.body.LenderOptIn,
+    Address: req.body.Address,
+    City: req.body.City,
+    State: req.body.State,
+    ZipCode: req.body.ZipCode,
+    tags: req.body.tags,
+    Link: req.body.Link,
+    Birthday: req.body.Birthday,
+    HomeClosingDate: req.body.HomeClosingDate,
+  });
+  const response = await lead.save();
+  res.send(200, response);
+});
 
 app.post("/sms", (req, res) => {
   const twiml = new MessagingResponse();
