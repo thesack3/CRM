f6756e5e566840af0b2357b64912aee6bf7d3258
devbyishtiaq <ishtiaaq.mushtaaq@gmail.com>
2023-05-24 20:28:33 +0500

    task update/delete functionality

--- client/src/mutations/reminder.js
+++ client/src/mutations/reminder.js
@@ -1,4 +1,4 @@
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
 export const ADD_TASK = gql`
   mutation AddTASK($title: String, $note: String, $date: String, $type: String) {
@@ -7,6 +7,26 @@ export const ADD_TASK = gql`
       note
       date
       type
+    }
+  }
+`;
+
+export const UPDATE_TASK = gql`
+  mutation UpateTask($id: ID, $title: String, $note: String, $date: String, $type: String) {
+    updateTask(id: $id, title: $title, note: $note, date: $date, type: $type) {
+      id
+      title
+      note
+      date
+      type
+    }
+  }
+`;
+
+export const DELETE_TASK = gql`
+  mutation DeleteTask($id: ID) {
+    deleteTask(id: $id) {
+      id
     }
   }
 `;
