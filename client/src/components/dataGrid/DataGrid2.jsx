4450117f7ed1fd4c36a1969964cbe43cdc74a9e7
aliraza2u <raza8r@gmail.com>
2023-06-16 17:12:29 +0500

    fixed category scroll

--- client/src/components/dataGrid/DataGrid2.jsx
+++ client/src/components/dataGrid/DataGrid2.jsx
@@ -963,7 +963,7 @@ export default function DataGridProCSV2() {
       </div>
       <div style={{ height: 690, width: '100%' }}>
         <Box sx={{ marginTop: '16px' }}>
-          <Box sx={{ width: '70vw', display: 'inline-flex', overflow: 'scroll', gap: '20px' }}>
+          <Box sx={{ width: '70vw', paddingBottom: '.5rem', display: 'inline-flex', overflow: 'scroll', gap: '20px' }}>
             {categoriesList?.categories?.map((category) => (
               <Box>
                 <CategoryInput
